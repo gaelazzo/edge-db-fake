13fd0c8a418c3c4cee7874d06618310e10eaf2eb
gaelazzo <gaetano.lazzo@gmail.com>
2021-03-05 09:42:30 +0100

    Aggiungere i file di progetto.

---
+++ edge-db-fake/EdgeCompiler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace edge_db_fake {
+    public class EdgeCompiler {
+        public Func<object, Task<object>> CompileFunc(IDictionary<string, object> parameters) { 
+            return (o) => { return Task.FromResult((object) null); };
+        }
+    }
+}
